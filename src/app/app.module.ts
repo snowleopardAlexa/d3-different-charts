844aaf0a159fda16e0b7f63d6af2b66e9fb6447a
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-12 13:30:41 -0500

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PieComponent } from './pie/pie.component';
+import { BarComponent } from './bar/bar.component';
+import { ScatterComponent } from './scatter/scatter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieComponent,
+    BarComponent,
+    ScatterComponent
   ],
   imports: [
     BrowserModule
